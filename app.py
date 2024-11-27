a04f66fa88a7d03e1bff92fced26d2a35453e490
@PyAdminRU <zh.vitaliy92@gmail.com>
2024-11-28 00:15:44 +0300

    Добавлен выбор упражнения.

--- app.py
+++ app.py
@@ -2,35 +2,35 @@ from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
-
 @app.route('/')
 def index():
-    """Стартовая страница"""
+    """Стартовая страница с выбором дня недели"""
     return render_template('index.html')
 
+@app.route('/types_of_exercises/<day>')
+def types_of_exercises(day):
+    """Выбор упражнения с учетом выбранного дня"""
+    return render_template('types_of_exercises.html', day=day)
 
-@app.route('/exercise/<day>', methods=['GET', 'POST'])
-def exercise(day):
-    """Страница ввода тренировки"""
+@app.route('/exercise/<day>/<exercise1>', methods=['GET', 'POST'])
+def exercise(day, exercise1):
+    """Страница ввода данных о подходах"""
     if request.method == 'POST':
         sets = int(request.form['sets'])
-        return render_template('workout_calculation.html', sets=sets, day=day)
-    return render_template('exercise.html', day=day)
-
+        return render_template('workout_calculation.html', sets=sets, day=day, exercise1=exercise1)
+    return render_template('exercise.html', day=day, exercise1=exercise1)
 
 @app.route('/workout_calculation', methods=['POST'])
 def workout_data():
-    """Получение данных о подходах и их сохранение"""
+    """Получение данных о подходах и их расчет"""
     total_weight = 0
-    # Получаем все веса и повторения из формы
-    sets = int(request.form['sets'])  # Количество подходов
+    sets = int(request.form['sets'])
     for i in range(sets):
-        weight = float(request.form[f'set[{i}][weight]'])  # Вес
-        reps = int(request.form[f'set[{i}][reps]'])  # Повторения
+        weight = float(request.form[f'set[{i}][weight]'])
+        reps = int(request.form[f'set[{i}][reps]'])
         total_weight += weight * reps
 
     return render_template('workout_result.html', total_weight=total_weight)
-
 
 if __name__ == '__main__':
     app.run(debug=True)

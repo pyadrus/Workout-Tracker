5c9e0a7c3e625dc97f9c52833cbb4c6019f1602a
@PyAdminRU <zh.vitaliy92@gmail.com>
2024-11-28 00:42:48 +0300

    Рефакторинг кода.

--- app.py
+++ app.py
@@ -2,15 +2,18 @@ from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
+
 @app.route('/')
 def index():
     """Стартовая страница с выбором дня недели"""
     return render_template('index.html')
 
+
 @app.route('/types_of_exercises/<day>')
 def types_of_exercises(day):
     """Выбор упражнения с учетом выбранного дня"""
     return render_template('types_of_exercises.html', day=day)
+
 
 @app.route('/exercise/<day>/<exercise1>', methods=['GET', 'POST'])
 def exercise(day, exercise1):
@@ -19,6 +22,7 @@ def exercise(day, exercise1):
         sets = int(request.form['sets'])
         return render_template('workout_calculation.html', sets=sets, day=day, exercise1=exercise1)
     return render_template('exercise.html', day=day, exercise1=exercise1)
+
 
 @app.route('/workout_calculation', methods=['POST'])
 def workout_data():
@@ -31,6 +35,7 @@ def workout_data():
         total_weight += weight * reps
 
     return render_template('workout_result.html', total_weight=total_weight)
+
 
 if __name__ == '__main__':
     app.run(debug=True)

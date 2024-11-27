80ec0cfa77269641774f3642e36782d63446de06
@PyAdminRU <zh.vitaliy92@gmail.com>
2024-11-27 23:49:35 +0300

    Рефакторинг кода.

--- app.py
+++ app.py
@@ -2,22 +2,25 @@ from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
-# Стартовая страница
+
 @app.route('/')
 def index():
+    """Стартовая страница"""
     return render_template('index.html')
 
-# Страница ввода тренировки
+
 @app.route('/exercise/<day>', methods=['GET', 'POST'])
 def exercise(day):
+    """Страница ввода тренировки"""
     if request.method == 'POST':
         sets = int(request.form['sets'])
         return render_template('workout_calculation.html', sets=sets, day=day)
     return render_template('exercise.html', day=day)
 
-# Страница для получения данных о подходах и их сохранения
+
 @app.route('/workout_calculation', methods=['POST'])
 def workout_data():
+    """Получение данных о подходах и их сохранение"""
     total_weight = 0
     # Получаем все веса и повторения из формы
     sets = int(request.form['sets'])  # Количество подходов
@@ -27,6 +30,7 @@ def workout_data():
         total_weight += weight * reps
 
     return render_template('workout_result.html', total_weight=total_weight)
+
 
 if __name__ == '__main__':
     app.run(debug=True)

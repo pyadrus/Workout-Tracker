df5ba806a9c94a43b07721e28cc5a393d20407d4
@PyAdminRU <zh.vitaliy92@gmail.com>
2024-12-04 16:56:15 +0300

    Изменение путей в index.html, с учетом библиотеки FastApi.

--- WebApp/main.py
+++ WebApp/main.py
@@ -6,11 +6,9 @@ import uvicorn
 
 app = FastAPI()
 
+app.mount("/static", StaticFiles(directory="static"), name="static")  # Подключаем статические файлы
 
-app.mount("/static", StaticFiles(directory="static"), name="static")# Подключаем статические файлы
-
-
-templates = Jinja2Templates(directory="templates")# Указываем директорию с шаблонами.
+templates = Jinja2Templates(directory="templates")  # Указываем директорию с шаблонами.
 
 
 @app.get("/", response_class=HTMLResponse)
@@ -21,9 +19,23 @@ async def index(request: Request):
     :param request: Объект запроса.
     :return: HTML-страница с приветствием.
     """
-    return templates.TemplateResponse("index.html", {"request": request})  # Добавлен context.
+    return templates.TemplateResponse("index.html", {"request": request})  # Добавлен context.view_results
 
+@app.get("/about_us")
+async def about(request: Request):
+    """
+    Отображение страницы с информацией о проекте.
+    :return: HTML-страница с информацией о проекте.
+    """
+    return templates.TemplateResponse("about_us.html", {"request": request})
 
+@app.get("/view_results")
+async def view_results(request: Request):
+    """
+    Отображение страницы с информацией о проекте.
+    :return: HTML-страница с информацией о проекте.
+    """
+    return templates.TemplateResponse("view_results.html", {"request": request})
 
 if __name__ == "__main__":
     uvicorn.run("main:app", host="127.0.0.1", port=8000, reload=True)

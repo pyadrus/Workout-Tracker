3462e055182ffcd4ce73ded62ff51ec7a80f87b9
evgenii <vovk.evgenii.199811@gmail.com>
2024-12-05 11:53:06 +0300

    рефакторинг кодаэ

--- handlers/start.py
+++ handlers/start.py
@@ -2,7 +2,7 @@
 # В этом файле создается логика обработки сообщений и FSM (Finite State Machine) для регистрации пользователей.
 
 import json
-
+from pathlib import Path
 from aiogram import F, Router
 from aiogram.filters import CommandStart
 from aiogram.types import CallbackQuery, Message
@@ -18,9 +18,16 @@ from keyboards.keyboards import (
 
 router = Router()  # Создание маршрутизатора для обработки команд и сообщений.
 
+
 # Чтение файла json для выборки текстов
-with open("data/text.json", "r", encoding="utf-8") as file:
-    texts = json.load(file)
+def load_text_form_file(file_name):
+    file_path = Path(f"messages/{file_name}")
+    if (
+        file_path.exists()
+    ):  # возвращает true , если объект файловой системы существует, и false – если нет
+        with open(file_path, "r", encoding="utf-8") as file:
+            return json.load(file)  # Загружаем строку текста
+    return "Файл не найден!"
 
 
 # Обработчик команды /start, отправляющий приветственное сообщение и клавиатуру с вариантами.
@@ -37,12 +44,12 @@ async def start_bot(message: Message) -> None:
     data_user = get_user_data(user_id)
     if not data_user:
         await message.answer(
-            f"👋 Приветствую тебя, @{username}{texts['text_hello_welcome']}",
+            f"👋 Приветствую тебя, @{username}{load_text_form_file('text_hello_welcome.json')}",
             reply_markup=generate_user_options_keyboard(),
         )
     else:
         await message.answer(
-            f"👋 Приветствую тебя, @{username}{texts['text_authorized_user_greeting']}",
+            f"👋 Приветствую тебя, @{username}{load_text_form_file('text_authorized_user_greeting.json')}",
             reply_markup=generate_authorized_user_options_keyboard(),
         )
 
@@ -56,6 +63,6 @@ async def description(callback_query: CallbackQuery) -> None:
     :param message: Сообщение пользователя с текстом "описание".
     """
     await callback_query.message.answer(
-        f"ℹ️ {texts['text_description']}",
+        f"ℹ️ {load_text_form_file('text_description.json')}",
         reply_markup=generate_authorized_user_discription(),
     )

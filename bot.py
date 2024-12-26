6ec5cf3ede29847af47917d18e7462db78eeff6b
@PyAdminRU <zh.vitaliy92@gmail.com>
2024-12-26 18:03:36 +0300

    Рефакторинг кода.

--- bot.py
+++ bot.py
@@ -1,19 +1,17 @@
 import asyncio
+import logging
+import sys
 
-from aiogram import Bot, Dispatcher
-from aiogram.client.default import DefaultBotProperties
-from aiogram.enums.parse_mode import ParseMode
 from loguru import logger
 
-from data.config import BOT_TOKEN  # Импорт токена бота из файла конфигурации.
-from handlers.admin import router
+from data.config import dp, bot  # Импорт токена бота из файла конфигурации.
 from handlers.exercises.main import register_exercises
 from handlers.exercises.muscles.biceps import register_biceps
 from handlers.exercises.muscles.pectoral import register_pectoral
 from handlers.exercises.muscles.triceps import register_triceps
-from handlers.user.today import register_today
 from handlers.user.help import register_help
 from handlers.user.menu import register_menu
+from handlers.user.today import register_today
 from handlers.user.training import register_training
 from handlers.user.workout import register_workout
 
@@ -24,12 +22,9 @@ async def start_bot() -> None:
 
     Создает экземпляр бота, регистрирует маршрутизаторы и запускает процесс опроса обновлений (polling).
     """
+    logging.basicConfig(level=logging.INFO, stream=sys.stdout)
     try:
-        bot = Bot(token=BOT_TOKEN, default=DefaultBotProperties(parse_mode=ParseMode.HTML))
-        # Создание диспетчера для управления маршрутизацией и обработкой событий.
-        dp = Dispatcher()
-        # Подключение маршрутизаторов с обработчиками команд и сообщений.
-        dp.include_router(router)
+        await dp.start_polling(bot)
 
         register_triceps()  # Упражнения на трицепс
         register_biceps()  # Упражнения на бицепс

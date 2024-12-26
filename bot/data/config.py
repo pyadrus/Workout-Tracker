f998db61250a40d76f7fc7d04764d32402a1acb6
@PyAdminRU <zh.vitaliy92@gmail.com>
2024-12-26 17:44:27 +0300

    Рефакторинг кода.

--- bot/data/config.py
+++ bot/data/config.py
@@ -7,30 +7,21 @@ from dotenv import load_dotenv
 from loguru import logger
 
 # Загрузка переменных окружения
-load_dotenv(dotenv_path="system/.env")
+load_dotenv(dotenv_path="system/.env")  # Загружаем переменные из .env файла
 
-BOT_TOKEN = os.getenv("BOT_TOKEN")
+BOT_TOKEN = os.getenv("BOT_TOKEN")  # Получаем токен бота из переменной окружения
 if not BOT_TOKEN:
     logger.error("BOT_TOKEN не найден в .env файле!")
     sys.exit("Укажите BOT_TOKEN в .env файле.")
-
-# Инициализация бота, диспетчера и хранилища
-bot = Bot(token=BOT_TOKEN)
-storage = MemoryStorage()
-dp = Dispatcher(storage=storage)
-
-# Создание маршрутизатора
-router = Router()
-dp.include_router(router)
-
-load_dotenv()  # Загружаем переменные из .env файла
-
-BOT_TOKEN = os.getenv("TOKEN")  # Получаем токен бота из переменной окружения
+logger.info(BOT_TOKEN)  # Проверка, что токен загружен
 
 ADMIN_USER_ID = os.getenv("ADMIN_ID")
 
 BASE_SITE_URL = os.getenv("BASE_SITE")
 
-logger.info(BOT_TOKEN)  # Проверка, что токен загружен
-
+# Инициализация бота, диспетчера и хранилища
+bot = Bot(token=BOT_TOKEN)
+storage = MemoryStorage()
+dp = Dispatcher(storage=storage)
 router = Router()  # Создание маршрутизатора для обработки команд и сообщений.
+dp.include_router(router)
